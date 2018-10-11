e8eb50cb4492534397e19ee72f24874815ba66b0
Nandish <13392242+nand6m@users.noreply.github.com>
2018-10-11 17:02:45 -0500

    Update StreamMsg.java

--- src/StreamMsg.java
+++ src/StreamMsg.java
@@ -1,6 +1,6 @@
 import java.io.Serializable;
 import java.util.ArrayList;
-enum MsgType{neighbor,okay,terminate,PACK,NACK,parentRequest, broadcast};
+enum MsgType{neighbor,okay,terminate,PACK,NACK,parentRequest, broadcast, convergeCast_ack};
 public class StreamMsg implements Serializable {
 	int sourceNodeId;	
 	int immediateSourceNodeId;
